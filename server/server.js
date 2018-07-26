acd3b2f341c44b39afd887007263d5e01a0b22ec
Tino <tinozhang17@gmail.com>
2018-07-26 10:08:24 -0400

    ESlint

--- server/server.js
+++ server/server.js
@@ -1,156 +1,149 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+const { ObjectID } = require('mongodb');
 const _ = require('lodash');
-const config = require('./config/config');
+require('./config/config');
 
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
-const {authenticate, authenticateLogin} = require('./middleware/authenticate');
+require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+const { authenticate } = require('./middleware/authenticate');
 
 const PORT = process.env.PORT;
 
-let app = express();
+const app = express();
 
 app.use(bodyParser.json());
 
 app.get('/todos', authenticate, (req, res) => {
-    Todo.find({
-        _creator: req.user._id
-    }).then((todos) => {
-        res.send({todos});
-    }, (err) => {
-        res.status(400).send(err);
-    });
+  Todo.find({
+    _creator: req.user._id,
+  }).then((todos) => {
+    res.send({ todos });
+  }, (err) => {
+    res.status(400).send(err);
+  });
 });
 
 app.post('/todos', authenticate, (req, res) => {
-    let todo = new Todo({
-        text: req.body.text,
-        _creator: req.user._id
-    });
-    todo.save((err, result) => {
-        if (err) {
-            return res.status(400).send(err);
-        }
-        res.send(result);
-    });
+  const todo = new Todo({
+    text: req.body.text,
+    _creator: req.user._id,
+  });
+  todo.save((err, result) => {
+    if (err) {
+      return res.status(400).send(err);
+    }
+    res.send(result);
+  });
 });
 
 // GET /todos/123456
 app.get('/todos/:id', authenticate, (req, res) => {
-    let id = req.params.id;
-    if (!ObjectID.isValid(id)) {
-        return res.status(404).send();
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findOne({
+    _id: id,
+    _creator: req.user._id,
+  }).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
     }
-    Todo.findOne({
-        _id: id,
-        _creator:req.user._id
-    }).then((todo) => {
-        if (!todo) {
-            return res.status(404).send();
-        }
-        res.send({todo});
-    }, (err) => {
-        res.status(400).send();
-    });
+    res.send({ todo });
+  }, () => {
+    res.status(400).send();
+  });
 });
 
 app.delete('/todos/:id', authenticate, async (req, res) => {
-    // get the id
-    const id = req.params.id;
-    // validate the id
-    if (!ObjectID.isValid(id)) {
-        return res.status(404).send();
-    }
+  // get the id
+  const id = req.params.id;
+  // validate the id
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
-    try {
-        const todo = await Todo.findOneAndRemove({
-            _id: id,
-            _creator: req.user._id
-        });
-        if (!todo) {
-            return res.status(404).send();
-        }
-        return res.send({todo});
-    } catch (e) {
-        return res.status(400).send();
+  try {
+    const todo = await Todo.findOneAndRemove({
+      _id: id,
+      _creator: req.user._id,
+    });
+    if (!todo) {
+      return res.status(404).send();
     }
+    return res.send({ todo });
+  } catch (e) {
+    return res.status(400).send();
+  }
 });
 
 app.patch('/todos/:id', authenticate, (req, res) => {
-    let id = req.params.id;
-    let body = _.pick(req.body, ['text', 'completed']);
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']);
 
-    if (!ObjectID.isValid(id)) {
-        return res.status(404).send();
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  // findOneAndUpdate(id, update operators, options)
+  // here, {new: true} means return the updated document, not the original document
+  Todo.findOneAndUpdate({ _id: id, _creator: req.user._id }, { $set: body }, { new: true }).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
     }
-
-    if (_.isBoolean(body.completed) && body.completed) {
-        body.completedAt = new Date().getTime();
-    } else {
-        body.completed = false;
-        body.completedAt = null;
-    }
-
-    // findOneAndUpdate(id, update operators, options)
-    // here, {new: true} means return the updated document, not the original document
-    Todo.findOneAndUpdate({_id: id, _creator: req.user._id}, {$set: body}, {new: true}).then((todo) => {
-        if (!todo) {
-            return res.status(404).send();
-        }
-        res.send({todo});
-    }).catch((err) => {
-        return res.status(400).send();
-    });
+    res.send({ todo });
+  }).catch(() => res.status(400).send());
 });
 
 // POST /users
 app.post('/users', async (req, res) => {
-    const body = _.pick(req.body, ['email', 'password']);
-    const user = new User(body);
+  const body = _.pick(req.body, ['email', 'password']);
+  const user = new User(body);
 
-    try {
-        await user.save();
-        const token = user.generateAuthToken();
-        return res.header('x-auth', token).send(user); // when you tack on an "x-" before a header, you are creating a custom header, which means it's not necessarily a header that HTTP supports by default. In our example, we are using a jwt token scheme, so we are creating a custom header to store that value.
-    } catch (e) {
-        res.status(400).send(e);
-    }
+  try {
+    await user.save();
+    const token = user.generateAuthToken();
+    return res.header('x-auth', token).send(user); // when you tack on an "x-" before a header, you are creating a custom header, which means it's not necessarily a header that HTTP supports by default. In our example, we are using a jwt token scheme, so we are creating a custom header to store that value.
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
 app.get('/users/me', authenticate, (req, res) => {
-    res.send(req.user);
+  res.send(req.user);
 });
 
 app.post('/users/login', async (req, res) => {
-    try {
-        const body = _.pick(req.body, ['email', 'password']);
-        const user = await User.findByCredentials(body.email, body.password);
-        const token = await user.generateAuthToken();
-        return res.header('x-auth', token).send(user);
-    } catch (e) {
-        res.status(400).send();
-    }
+  try {
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = await User.findByCredentials(body.email, body.password);
+    const token = await user.generateAuthToken();
+    return res.header('x-auth', token).send(user);
+  } catch (e) {
+    res.status(400).send();
+  }
 });
 
 app.delete('/users/me/token', authenticate, async (req, res) => {
-    try {
-        await req.user.removeToken(req.token); // we don't care about the return value here, as long as it resolves. If it rejects, then the catch block will run.
-        res.status(200).send();
-    } catch (e) {
-        res.status(400).send();
-    }
+  try {
+    await req.user.removeToken(req.token); // we don't care about the return value here, as long as it resolves. If it rejects, then the catch block will run.
+    res.status(200).send();
+  } catch (e) {
+    res.status(400).send();
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
 
-module.exports = {app};
-
-
-
-
-
+module.exports = { app };
